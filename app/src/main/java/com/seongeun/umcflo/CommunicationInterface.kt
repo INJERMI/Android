4b8f17e38efea66a5a5a8531e850954342e63970
INGERMI <jsj66150207@gmail.com>
2024-11-18 18:25:54 +0900

    test2

--- app/src/main/java/com/seongeun/umcflo/CommunicationInterface.kt
+++
@@ -1,5 +0,0 @@
-package com.seongeun.umcflo
-
-interface CommunicationInterface {
-    fun sendData(album: Album)
-}
--- app/src/main/java/com/seongeun/umcflo/Constant.kt
+++
@@ -1,6 +0,0 @@
-package com.seongeun.umcflo
-
-object Constant {
-    const val CHANNEL_ID = "ch123" // 아무런 값이나 입력한다.
-    const val MUSIC_NOTIFICATION_ID = 123 // 아무런 값이나 입력한다.
-}
